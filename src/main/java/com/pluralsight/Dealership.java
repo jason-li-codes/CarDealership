a9d082f623f398504b859c3af331e30bbeb7ccdd
lijas <lijason991@gmail.com>
2025-10-23 21:03:41 -0400

    first commit

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,74 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealership {
+
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<Vehicle> getVehicleByPrice(double minPrice, double maxPrice) {
+    }
+
+    public List<Vehicle> getVehicleByMakeModel(String make, String model) {
+    }
+
+    public List<Vehicle> getVehicleByYear(int minYear, int maxYear) {
+    }
+
+    public List<Vehicle> getVehicleByColor(String color) {
+    }
+
+    public List<Vehicle> getVehicleByMileage(int minMileage, int maxMileage) {
+    }
+
+    public List<Vehicle> getVehicleByType(String vehicleType) {
+    }
+
+    public List<Vehicle> getAllVehicles() {
+        return this.inventory;
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+        this.inventory.add(vehicle);
+    }
+
+    public void removeVehicle(Vehicle vehicle) {
+        this.inventory.remove(vehicle);
+    }
+
+}
