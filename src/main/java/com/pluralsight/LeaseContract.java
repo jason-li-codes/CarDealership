c5304663feba5d160715ba8a69a29ec08841d57b
lijas <lijason991@gmail.com>
2025-10-31 21:35:12 -0400

    defined abstract class methods

--- src/main/java/com/pluralsight/LeaseContract.java
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -8,15 +8,30 @@ public class LeaseContract extends Contract {
         super(contractDate, customerName, customerEmail, vehicleSold);
     }
 
+    public double getExpectedEndingValue() {
+        double endingValuePercentage = 50;
+        return vehicleSold.getPrice() * (endingValuePercentage / 100);
+    }
+
+    public double getLeaseFee() {
+        double leaseFeePercentage = 7;
+        return vehicleSold.getPrice() * (leaseFeePercentage / 100);
+    }
+
     @Override
     public double getTotalPrice() {
-
-        return 0;
+        return vehicleSold.getPrice() - getExpectedEndingValue() + getLeaseFee();
     }
 
     @Override
     public double getMonthlyPayment() {
-        return 0;
+        double interestAnnual = 4;
+        double loanLengthYears = 3;
+        double interest = interestAnnual / 100 / 12;
+        double loanLengthMonths = loanLengthYears * 12;
+        return (vehicleSold.getPrice() - getExpectedEndingValue()) *
+                ((interest * Math.pow((1 + interest), loanLengthMonths)) /
+                (Math.pow((1 + interest), loanLengthMonths) - 1));
     }
 
 }
