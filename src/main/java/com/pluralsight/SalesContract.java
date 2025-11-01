f29e98da5e6afdacde15b729ed762bbcf78c20dc
lijas <lijason991@gmail.com>
2025-10-31 21:08:11 -0400

    defined abstract class methods

--- src/main/java/com/pluralsight/SalesContract.java
+++ src/main/java/com/pluralsight/SalesContract.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 public class SalesContract extends Contract {
 
     private boolean isFinanced;
+    private boolean isPriceHigh = vehicleSold.getPrice() >= 10000;
 
     public SalesContract(LocalDate contractDate, String customerName,
                          String customerEmail, Vehicle vehicleSold, boolean isFinanced) {
@@ -12,15 +13,36 @@ public class SalesContract extends Contract {
         this.isFinanced = isFinanced;
     }
 
-    @Override
-    public double getTotalPrice() {
-
-        return 0;
+    public double getSalesTax() {
+        double salesTax = 0.05;
+        return vehicleSold.getPrice() * salesTax;
     }
 
     @Override
+    public double getTotalPrice() {
+
+        double recordingFee = 100;
+        double processingFee = (isPriceHigh) ? 495 : 295;
+
+        return vehicleSold.getPrice() + getSalesTax() + recordingFee + processingFee;
+        }
+
+    @Override
     public double getMonthlyPayment() {
-        return 0;
+
+        if (isFinanced) {
+            double principal = vehicleSold.getPrice();
+            double interestAnnual = (isPriceHigh) ? 4.25 : 5.25;
+            double loanLengthYears = (isPriceHigh) ? 4 : 2;
+            // changes interest to a decimal and loan length to be in months
+            double interest = interestAnnual / 100 / 12;
+            double loanLengthMonths = loanLengthYears * 12;
+            // calculates monthly payment
+            return principal * ((interest * Math.pow((1 + interest), loanLengthMonths)) /
+                    (Math.pow((1 + interest), loanLengthMonths) - 1));
+        } else {
+            return 0;
+        }
     }
 
 }
